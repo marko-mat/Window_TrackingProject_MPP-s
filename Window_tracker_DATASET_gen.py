974962b687ff96515fdf8b9767f7d8e575fbd1ff
Marko M <131008753+marko-mat@users.noreply.github.com>
2024-10-21 21:56:58 -0400

    Update Window_tracker_DATASET_gen.py

--- Window_tracker_DATASET_gen.py
+++ Window_tracker_DATASET_gen.py
@@ -18,7 +18,7 @@ def get_active_window_process():
 
 
 # appending data to csv
-def write_to_csv(window_times, session_id, filename='window_time_tracking.csv'):
+def write_to_csv(window_times, session_id, productivefactor, filename='window_time_tracking.csv'):
     file_exists = False
     try:
         with open(filename, mode='r'):
@@ -30,13 +30,13 @@ def write_to_csv(window_times, session_id, filename='window_time_tracking.csv'):
         writer = csv.writer(file)
                                           #write header for newfiles
         if not file_exists:
-            writer.writerow(['Session ID', 'Application', 'Total Time (seconds)'])
+            writer.writerow(['Session ID', 'Application', 'Total Time (seconds)', 'Productive Factor'])
 
                                           # write with session id
         for window, total_time in window_times.items():
-            writer.writerow([session_id, window, f'{total_time:.2f}'])
+            writer.writerow([session_id, window, f'{total_time:.2f}', productivefactor])
 
-    print(f"\nTime tracking data saved to {filename} for session {session_id}")
+    print(f"\nTime tracking data saved to {filename} for session {session_id}" + productivefactor)
 
 
                                           # Time tracking
@@ -65,13 +65,15 @@ def monitor_active_window():
             time.sleep(1)
 
     except KeyboardInterrupt:
-        print("\nMonitoring stopped.")
+        productivity = input("\n")
         for window, total_time in window_times.items():
-            print(f"App '{window}' total time: {total_time:.2f} seconds.")
+            print(f"App '{window}' total time: {total_time:.2f} seconds")
 
         # Write data to CSV file with session ID
-        write_to_csv(window_times, session_id)
+        write_to_csv(window_times, session_id,productivity)
 
 
 if __name__ == "__main__":
+    print("Please remember, the FIRST number you type in the cosole, is your how productive your session is... \nPlease "
+          "input a number between 0 and 100 before you close the program!")
     monitor_active_window()
